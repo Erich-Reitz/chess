78b891a754135a75c0753892cc124f6390c42c65
Erich <erich.reitz12@gmail.com>
2023-07-13 19:59:19 -0400

    format

--- src/move_generation.hpp
+++ src/move_generation.hpp
@@ -4,12 +4,12 @@
 #include "PieceType.hpp"
 #include "Move.hpp"
 #include "Board.hpp"
+
 std::vector<Move> generateMovesInStraightLine(const Board *board, const ValidPosition& currentPosition, PieceColor pieceColor, const int row_difs[], const int col_difs[]) ;
 
 PieceColor opposite_color(PieceColor color);
 
 Move constructCastleMove(ValidPosition kingPosition, ValidPosition rookPosition, ValidPosition kingDest, ValidPosition rookDest, PieceColor pieceColor) ;
-
 
 std::vector<Move> generateAllDiagonalMoves(const Board *board, const ValidPosition& current, PieceColor color);
 
